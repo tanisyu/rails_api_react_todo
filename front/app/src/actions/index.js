2b3fb79b14a632050c8621eb09be0d008f2cdac3
tanisyu <tanisyu1997@gmail.com>
2021-01-19 22:33:19 +0900

    add create tasks and routes

--- front/app/src/actions/index.js
+++ front/app/src/actions/index.js
@@ -1,10 +1,16 @@
 import axios from 'axios'
 
 export const READ_TASKS = 'READ_TASKS'
+export const CREATE_TASK = 'CREATE_TASK'
 
 const ROOT_URL = 'http://127.0.0.1:3000/api/v1'
 
 export const readTasks = () => async dispatch => {
   const response = await axios.get(`${ROOT_URL}/tasks`)
   dispatch({ type: READ_TASKS, response })
+}
+
+export const postTask = values => async dispatch => {
+  const response = await axios.post(`${ROOT_URL}/tasks`, values)
+  dispatch({ type: CREATE_TASK, response })
 }
--- front/app/src/index.js
+++ front/app/src/index.js
@@ -6,8 +6,10 @@ import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import thunk from 'redux-thunk'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import TasksIndex from './components/tasks_index';
+import TasksNew from './components/tasks_new';
 
 const enhancer = applyMiddleware(thunk)
 const store = createStore(reducer, enhancer)
@@ -15,7 +17,13 @@ const store = createStore(reducer, enhancer)
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <TasksIndex />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={TasksIndex} />
+          <Route exact path="/tasks" component={TasksIndex} />
+          <Route path="/tasks/new" component={TasksNew} />
+        </Switch>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
